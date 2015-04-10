3e9536820cf56784fec46c0d627fcbf68e3ba3f8
Uhkis <hakala.janne@gmail.com>
2015-04-10 11:04:24 +0300

    Cache selections. Fix #3.

--- js/floatforms.js
+++ js/floatforms.js
@@ -5,34 +5,37 @@ $.fn.floatforms = function( options ) {
     boxClass: 'floatbox',
     animate: true,
   }, options);
-  $(settings.floatbox).addClass(settings.boxClass);
-  $form = $(this);
+  var $floatbox = $(settings.floatbox),
+      $form = $(this);
+
+  $floatbox.addClass(settings.boxClass);
   var speed = 0;
   if (settings.animate) {
     speed = 400;
   }
   $(this).find('input').on('blur focusout', function() {
-    if ($(this).attr('type') == 'submit') return;
-    if ($(this).val() == '') return;
+    var $this = $(this);
+    if ($this.attr('type') == 'submit') return;
+    if ($this.val() == '') return;
 
-    id = $(this).attr('id');
+    id = $this.attr('id');
     $container = $('<div>').attr('id', 'input-' + id).addClass('floatforms-moved');
 
-    $(this).fadeOut(speed);
-    $(this).off('blur focusout');
+    $this.fadeOut(speed);
+    $this.off('blur focusout');
     var label = $('<span>').addClass('input-label');
     label.text($('label[for=' + id + ']').text() + ': ');
     var value = $('<span>').addClass('input-value');
     value.text($(this).val());
 
     $container.append(label, value);
-    $(this).prev('label').fadeOut(speed, function() {
+    $this.prev('label').fadeOut(speed, function() {
       $(this).remove();
     });
     $container.append($(this).clone().attr('id', id+'-copy').hide()).fadeIn(speed);
-    $(settings.floatbox).append($container);
+    $floatbox.append($container);
   });
-  $(settings.floatbox).on('click', '.floatforms-moved', function() {
+  $floatbox.on('click', '.floatforms-moved', function() {
     var input = $(this).find('input');
     var text = $(this).find('.input-value');
     input.show();
